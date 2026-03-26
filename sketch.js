eb1752c408ce9e8ef5690db9f25791b932fb83e9
ocasoni <o.casoni@ied.edu>
2026-03-26 17:08:55 +0100

    linea retta fatta da cerchi

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let SIZE = 150;
+let NUMBER_OF_CIRCLES = 200;
 
-function draw() {
-  background(220);
+
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+
+  noStroke();
+  
+  // for (valore di partenza; condizione per cui il for va avanti; come cambia il valore ad ogni ciclo) {)
+  for (let count = 0; count < NUMBER_OF_CIRCLES; count+=1) {
+    // operazioni 
+    let progress = count / NUMBER_OF_CIRCLES; // valore 
+
+    let x = width * progress;
+    let y = height/2;
+    circle (x, y, SIZE);
+}
 }
